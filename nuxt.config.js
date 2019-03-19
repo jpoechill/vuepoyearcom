5f4847ec45c873cdbe1c92e15e538c6c435f3592
Po Rith <poo@Poamrongs-MBP.attlocal.net>
2019-03-19 11:47:29 -0700

    update description

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Poyear is a cupcake and macaron baker from Oakland, Ca.'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
